83fb012990dc14d9261ab24ed3546c1934438eeb
theghostuchiha <113781720+theghostuchiha@users.noreply.github.com>
2023-01-01 13:03:41 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,188 @@
+// itachiShisui
+// jirayaSensei
+// ladyTsuna
+// kakashi
+// naruto
+// kakashiRin
+// obito
+// narutoSasuke
+// kakashiObito
+
+document.getElementById('itachiShisui').addEventListener('click', imgItachiShisui);
+document.getElementById('jirayaSensei').addEventListener('click', imgJirayaSensei);
+document.getElementById('ladyTsuna').addEventListener('click', imgLadyTsuna);
+document.getElementById('coldItachi').addEventListener('click', imgColdItachi);
+document.getElementById('kidNaruto').addEventListener('click', imgKidNaruto)
+document.getElementById('kakashi').addEventListener('click', imgKakashi);
+document.getElementById('naruto').addEventListener('click', imgNaruto);
+document.getElementById('kakashiRin').addEventListener('click', imgKakashiRin);
+document.getElementById('hokageTsuna').addEventListener('click', imgHokageTsuna);
+document.getElementById('obitoMinato').addEventListener('click', imgObitoMinato);
+document.getElementById('obito').addEventListener('click', imgObito);
+document.getElementById('narutoSasuke').addEventListener('click', imgNarutoSasuke);
+document.getElementById('kakashiObito').addEventListener('click', imgKakashiObito);
+document.getElementById('naruHina').addEventListener('click', imgNaruHina);
+document.getElementById('Madara').addEventListener('click', imgMadara)
+
+function imgItachiShisui(){
+  document.querySelector('body').style.backgroundImage = "url('img/itachishisui.jpg')"
+  document.querySelector('div').style.backgroundImage = "url('img/itachishisui.jpg')"
+}
+function imgJirayaSensei(){
+  document.querySelector('body').style.backgroundImage = "url('./img/JirayaSensei.jpg')"
+  document.querySelector('div').style.backgroundImage = "url('./img/JirayaSensei.jpg')"
+}
+function imgLadyTsuna(){
+  document.querySelector('body').style.backgroundImage = "url('./img/LadyTsuna.jpg')"
+  document.querySelector('div').style.backgroundImage = "url('./img/LadyTsuna.jpg')"
+}
+function imgColdItachi(){
+  document.querySelector('body').style.backgroundImage = "url('./img/coldItachi.jpg')"
+  document.querySelector('div').style.backgroundImage = "url('./img/coldItachi.jpg')"
+}
+function imgKidNaruto(){
+  document.querySelector('body').style.backgroundImage = "url('./img/narutoKid.jpg')"
+  document.querySelector('div').style.backgroundImage = "url('./img/narutoKid.jpg')"
+}
+function imgKakashi(){
+  document.querySelector('body').style.backgroundImage = "url('img/kakashi.jpg')"
+  document.querySelector('div').style.backgroundImage = "url('img/kakashi.jpg')"
+}
+function imgNaruto(){
+  document.querySelector('body').style.backgroundImage = "url('img/Naruto.jpg')"
+  document.querySelector('div').style.backgroundImage = "url('img/Naruto.jpg')"
+}
+function imgKakashiRin(){
+  document.querySelector('body').style.backgroundImage = "url('img/kakashi rin.jpg')"
+  document.querySelector('div').style.backgroundImage = "url('img/kakashi rin.jpg')"
+}
+function imgHokageTsuna(){
+  document.querySelector('body').style.backgroundImage = "url('img/hokageTsuna.jpg')"
+  document.querySelector('div').style.backgroundImage = "url('img/hokageTsuna.jpg')"
+}
+function imgObitoMinato(){
+  document.querySelector('body').style.backgroundImage = "url('./img/obitoMinato.jpg')"
+  document.querySelector('div').style.backgroundImage = "url('./img/obitoMinato.jpg')"
+}
+function imgObito(){
+  document.querySelector('body').style.backgroundImage = "url('img/obito.jpg')"
+  document.querySelector('div').style.backgroundImage = "url('img/obito.jpg')"
+}
+function imgNarutoSasuke(){
+  document.querySelector('body').style.backgroundImage = "url('img/Naruto Sasuke.jpg')"
+  document.querySelector('div').style.backgroundImage = "url('img/Naruto Sasuke.jpg')"
+}
+function imgKakashiObito(){
+  document.querySelector('body').style.backgroundImage = "url('img/kakashi-obito.jpg')"
+  document.querySelector('div').style.backgroundImage = "url('img/kakashi-obito.jpg')"
+}
+function imgNaruHina(){
+  document.querySelector('body').style.backgroundImage = "url('img/naruHina.jpg')"
+  document.querySelector('div').style.backgroundImage = "url('img/naruHina.jpg')"
+}
+function imgMadara(){
+  document.querySelector('body').style.backgroundImage = "url('img/Madara.jpg')"
+  document.querySelector('div').style.backgroundImage = "url('img/Madara.jpg')"
+}
+
+
+// document.getElementById('itachiShisui').onclick = partyPurple
+// document.getElementById('green').onclick = partyGreen
+// document.getElementById('blue').onclick = partyBlue
+// document.getElementById('orange').onclick = partyOrange
+// document.getElementById('palegolden').onclick = partyPaleGolden
+// document.getElementById('crimson').onclick = partyCrimson
+
+// function partyPurple() {
+// document.querySelector('body').style.backgroundImage = "url('./img/Itachi-shisui.jpg')"
+//   document.querySelector('body').style.backgroundColor = 'rgba(241,63,247,1)'
+//   document.querySelector('body').style.color = 'white'
+// }
+
+// function partyGreen() {
+//   document.querySelector('body').style.backgroundColor = 'rgba(0,253,81,1)'
+//   document.querySelector('body').style.color = 'white'
+// }
+
+// function partyBlue() {
+//   document.querySelector('body').style.backgroundColor = 'rgb(5, 116, 241)'
+//   document.querySelector('body').style.color = 'white'
+// }
+
+// function partyOrange() {
+//   document.querySelector('body').style.backgroundColor = 'rgb(227, 153, 13)'
+//   document.querySelector('body').style.color = 'white'
+// }
+
+// function partyPaleGolden() {
+//   document.querySelector('body').style.backgroundColor = 'palegoldenrod'
+//   document.querySelector('body').style.color = 'white'
+// }
+
+// function partyCrimson () {
+//   document.querySelector('body').style.backgroundColor = 'crimson'
+//   document.querySelector('body').style.color = 'white'
+// }
+
+// 'rgba(241,63,247,1)' '#purple'
+// 'rgba(0,253,81,1)'   '#green'
+// 'rgb(5, 116, 241)'   '#blue'
+// 'rgb(227, 153, 13)'  '#orange'
+// 'palegoldenrod'
+// 'crimson'
+
+// document.getElementById('itachi-shisui').addEventListener('click', colorPurple)
+// document.getElementById('blue').addEventListener('click', colorBlue)
+// document.getElementById('green').addEventListener('click', colorGreen)
+// document.getElementById('orange').addEventListener('click', colorOrange)
+// document.getElementById('palegolden').addEventListener('click', colorPaleGoldenRod)
+// document.getElementById('crimson').addEventListener('click', colorCrimson)
+
+// function colorPurple(){
+//   document.querySelector('body').style.backgroundImage = "url('./img/Itachi-shisui.jpg')"
+//   document.querySelector('body').style.color = 'red'
+//   // document.querySelector('ul').style.backgroundColor = 'rgb(5, 116, 241)'
+// }
+
+// function colorGreen(){
+//   document.querySelector('body').style.backgroundColor =  'rgba(0,253,81,1)'
+//   document.querySelector('body').style.color = '#FFF'
+//   document.querySelector('ul').style.backgroundColor =  'rgb(227, 153, 13)'
+// }
+
+// function colorBlue(){
+//   document.querySelector('body').style.backgroundColor =  'rgb(5, 116, 241)'
+//   document.querySelector('body').style.color = '#FFF'
+//   document.querySelector('ul').style.backgroundColor = 'palegoldenrod'
+// }
+
+// function colorOrange(){
+//   document.querySelector('body').style.backgroundColor = 'rgb(227, 153, 13)'
+//   document.querySelector('body').style.color = '#FFF'
+//   document.querySelector('ul').style.backgroundColor = 'crimson'
+// }
+
+// function colorPaleGoldenRod(){
+//   document.querySelector('body').style.backgroundColor = 'palegoldenrod'
+//   document.querySelector('body').style.color ='#fff'
+//   document.querySelector('ul').style.backgroundColor = 'rgba(241,63,247,1)'
+// }
+
+// function colorCrimson(){
+//   document.querySelector('body').style.backgroundColor = 'crimson'
+//   document.querySelector('body').style.color = '#FFF'
+//   document.querySelector('ul').style.backgroundColor = 'rgba(0,253,81,1)'
+// }
+
+// itachiSshisui
+// jirayaSensei
+// kakashiObito
+// ladyTsuna
+// naruto
+// kakashi
+// obito
+// kakashiRin
+// narutoSasuke
+
+// document.getElementById('itachiShisui').addEventListener('click', img1)
+// document.getElementById('itachiShisui').addEventListener('click', img1);
